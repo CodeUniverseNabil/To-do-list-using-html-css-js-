bc8624c3875120526e957773cc06001337a2b990
CodeUniverseNabil <nabilsikder00@gmail.com>
2026-04-05 01:03:05 +0600

    add js

--- script.js
+++ script.js
@@ -1,10 +1,33 @@
-function getInput(){
-    let value = document.getElementById("input_work").value;
-    let li =document.createElement("li")
-    if (value.length>=4){
-        
-        document.getElementsByClassName("work_list").appendChild(li.innerText=value)
-    }else{
+function getInput() {
+    let work = document.getElementById("input_work");
+    let li = document.createElement("li")
+    let data = work.value;
+    if (data.length >= 4) {
+        document.getElementsByClassName("work_list")[0].appendChild(li)
+        li.innerText = data
+        let delBtn = document.createElement("button")
+        delBtn.style.marginLeft = "10px";
+        delBtn.innerText = "Del";
+        delBtn.style.backgroundColor = "#e74c3c";
+        delBtn.style.color = "white";
+        delBtn.style.border = "none";
+        delBtn.style.borderRadius = "4px";
+        delBtn.style.padding = "2px 8px";
+        li.appendChild(delBtn)
+        delBtn.style.cursor = "pointer"
+
+
+        delBtn.onclick = function () {
+            li.remove()
+        }
+        work.value = "";
+    } else {
         alert("that work is too low")
+        wrok.value = "";
     }
+}
+
+function remove(){
+    let cll =document.getElementsByClassName("work_list")[0]
+    cll.innerHTML=""
 }
