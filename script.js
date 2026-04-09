0e14d23b26ed001db4ad86f02f5a5e4cca48f753
CodeUniverseNabil <nabilsikder00@gmail.com>
2026-04-09 16:22:55 +0600

    new feuture add

--- script.js
+++ script.js
@@ -1,14 +1,9 @@
-function saveTasks() {
-    let items = document.getElementById("work_list").querySelectorAll("li");
-    let tasks = [];
-    items.forEach(function (li) {
-        tasks.push(li.firstChild.textContent);
-    });
-    localStorage.setItem("tasks", JSON.stringify(tasks));
-}
+var count = 0
+// var len= 0
 function addTask(text) {
+    // list_have()
     let li = document.createElement("li");
-    li.innerText=text
+    li.innerText = text
     let delBtn = document.createElement("button");
     delBtn.style.marginLeft = "10px";
     delBtn.innerText = "Del";
@@ -18,12 +13,34 @@ function addTask(text) {
     delBtn.style.borderRadius = "4px";
     delBtn.style.padding = "2px 8px";
     delBtn.style.cursor = "pointer";
+    var doneBtn = document.createElement("button");
+    doneBtn.style.marginLeft = "10px";
+    doneBtn.innerText = "Done";
+    doneBtn.style.backgroundColor = "#2ecc71";
+    doneBtn.style.color = "white";
+    doneBtn.style.border = "none";
+    doneBtn.style.borderRadius = "4px";
+    doneBtn.style.padding = "2px 8px";
+    doneBtn.style.cursor = "pointer";
+    li.appendChild(doneBtn)
     li.appendChild(delBtn);
+    
+    doneBtn.onclick = function () {
+        doneBtn.remove()
+        
+        count += 1
+        document.getElementById("is").innerText = count
+        stroge()
+    }
     delBtn.onclick = function () {
         li.remove();
         saveTasks();
+        count -= 1
+        document.getElementById("is").innerText = count
+        stroge()
     };
     document.getElementById("work_list").appendChild(li);
+    
 }
 function getInput() {
     let work = document.getElementById("input_work");
@@ -37,6 +54,7 @@ function getInput() {
         work.value = "";
     }
 }
+
 function remove() {
     document.getElementById("work_list").innerHTML = "";
     localStorage.removeItem("tasks");
@@ -50,3 +68,34 @@ window.onload = function () {
         });
     }
 };
+
+function stroge() {
+    let allist = list_have()
+    
+    
+    
+    
+    
+    localStorage.setItem("baki ache ",ernai)
+    
+}
+
+
+function list_have() {
+    let that = JSON.parse(localStorage.getItem("tasks"))
+    let len = that.length
+    
+    stroge()
+    return len
+}
+
+function saveTasks() {
+    let items = document.getElementById("work_list").querySelectorAll("li");
+    let tasks = [];
+    items.forEach(function (li) {
+        tasks.push(li.firstChild.textContent);
+    });
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+    localStorage.setItem("done", count)
+}
+// console.log(len)
